8e49aea85a8597984411a1bec856c5c53c9c631b
Fayzullo <92411568+fayzullo1501@users.noreply.github.com>
2022-03-17 18:39:13 +0300

    weather_kotlin
    mobile application - kotlin

---
+++ app/src/main/java/com/example/weather_kotlin/HomePage.kt
@@ -0,0 +1,29 @@
+package com.example.weather_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class HomePage : AppCompatActivity() {
+
+    private lateinit var bottomNav: BottomNavigationView
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+        bottomNav = findViewById(R.id.bottomNav)
+        navController = findNavController(R.id.fragmentContainerView)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.id_home_fragment, R.id.id_map_fragment, R.id.id_settings_fragment))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNav.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/example/weather_kotlin/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.weather_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        handler = Handler()
+        handler.postDelayed({
+
+
+            val intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
+    }
+}
