f9b48151ac96d5df46e1bd264cdb8b9a04fe8e60
fayzullo1501 <92411568+fayzullo1501@users.noreply.github.com>
2022-03-31 18:29:56 +0300

    Weather_app
    Kotlin mobile application

--- app/src/main/java/com/example/weather_kotlin/Fragments/FragmentHome.kt
+++ app/src/main/java/com/example/weather_kotlin/Fragments/FragmentHome.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weather_kotlin.R
 
 
@@ -17,6 +19,10 @@ class FragmentHome : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+
     }
+
 
 }
